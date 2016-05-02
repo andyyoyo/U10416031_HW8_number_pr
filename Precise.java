3ea7fbe18dd578c64f45409cb1405b6a83a867ba
andyyoyo <andyyoyo76543@gmail.com>
2016-05-03 01:10:07 +0800

    set the cases
    set the cases that can add, subtract,multiply and divide

--- Precise.java
+++ Precise.java
@@ -20,5 +20,23 @@ public class Precise {
 		BigDecimal n1 = new BigDecimal(a);
 		BigDecimal n2 = new BigDecimal(b);
 		
+		switch(c){
+		case 1:
+			System.out.println("Your answer is "+n1.add(n2));
+			break;
+		case 2:
+			System.out.println("Your answer is "+n1.subtract(n2));
+			break;
+		case 3:
+			System.out.println("Your answer is "+n1.multiply(n2));
+			break;
+		case 4:
+			System.out.println("Your answer is "+n1.divide(n2,DEF_DIV_SCALE,BigDecimal.ROUND_HALF_UP));
+			break;
+		default:
+			System.out.println("Error");
+			break;
+		}
+		
 	}	
 }	
